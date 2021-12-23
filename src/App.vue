492c7e4d24251f0bc674fed8e168eecb21d5f52b
lazlanrafar <lazlanrafar@gmail.com>
2021-12-24 00:20:51 +0700

    migrate to vuejs

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav-bar></nav-bar>
+
+    <main class="main">
+      <video
+        src="@/assets/img/video.mp4"
+        autoplay
+        loop
+        muted
+        type="mp4"
+      ></video>
+      <img src="@/assets/img/wrapper.jpg" class="wrapper" alt="" />
+      <h2>Ocean</h2>
+
+      <p class="copyright">Copyright © 2021 Latoe</p>
+      <ul class="sosmed">
+        <li>
+          <a href="#"><i class="fas fa-twitter"></i></a>
+        </li>
+        <li>
+          <a href="#"><i class="fas fa-github"></i></a>
+        </li>
+        <li>
+          <a href="#"><i class="fas fa-instagram"></i></a>
+        </li>
+      </ul>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    NavBar: () => import("@/components/NavBar.vue"),
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import "@/assets/css/app.scss";
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <header>
+      <a href="" class="logo">Logo</a>
+      <div class="rightSide">
+        <div class="btns dayNight" @click="dayNight()" ref="dayNight">
+          <i class="fas fa-sun"></i>
+          <i class="fas fa-moon"></i>
+        </div>
+        <div class="btns menuToggle" @click="menuToggle()" ref="menuToggle">
+          <i class="fas fa-bars"></i>
+          <i class="fas fa-times"></i>
+        </div>
+      </div>
+    </header>
+
+    <nav>
+      <ul class="navbar" ref="navbar">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Package</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  methods: {
+    dayNight() {
+      this.$refs.dayNight.classList.toggle("active");
+    },
+    menuToggle() {
+      this.$refs.menuToggle.classList.toggle("active");
+      this.$refs.navbar.classList.toggle("active");
+    },
+  },
+};
+</script>
